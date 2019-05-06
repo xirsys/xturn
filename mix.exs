c41b7c8b8cd31b4bbb2b33f6e179bdc27de17d34
Lee Sylvester <lee.sylvester@gmail.com>
2019-05-06 18:14:50 +0100

    Update to use external sockets lib

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Xirsys.XTurn.Mixfile do
     [
       app: :xturn,
       version: "0.1.0",
-      elixir: ">= 1.4.2",
+      elixir: ">= 1.6.6",
       name: "xturn",
       source_url: "https://github.com/xirsys/xturn",
       escript: [main_module: Xirsys.XTurn],
@@ -30,6 +30,7 @@ defmodule Xirsys.XTurn.Mixfile do
   defp deps() do
     [
       {:xmedialib, git: "https://github.com/xirsys/xmedialib"},
+      {:xturn_sockets, git: "https://github.com/xirsys/xturn-sockets"},
       {:xturn_cache, git: "https://github.com/xirsys/xturn-cache"},
       {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.18.3"},
