0b4771f6ca157930b78f76905e233bd5cefd9722
Lee Sylvester <lee.sylvester@gmail.com>
2019-07-05 19:02:49 +0100

    Reset mix.exs

--- mix.exs
+++ mix.exs
@@ -31,11 +31,9 @@ defmodule Xirsys.XTurn.Mixfile do
     [
       {:xmedialib,
        git: "https://github.com/xirsys/xmedialib",
-       ref: "dbbc2d91a9422c0e677ca8a71029383f847e7b2d",
        override: true},
       {:xturn_sockets,
        git: "https://github.com/xirsys/xturn-sockets",
-       ref: "fbb2a1cd17c7d895a91c855c3d062e09ecf01b32",
        override: true},
       {:xturn_cache, git: "https://github.com/xirsys/xturn-cache"},
       {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
