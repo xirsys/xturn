bc8718cb8c7a2d458426382b059fb3780f13203f
Lee Sylvester <lee.sylvester@gmail.com>
2019-07-19 16:32:01 +0100

    Use hex deployed dependencies

--- mix.exs
+++ mix.exs
@@ -29,10 +29,9 @@ defmodule Xirsys.XTurn.Mixfile do
 
   defp deps() do
     [
-      {:xmedialib, git: "https://github.com/xirsys/xmedialib", tag: "v0.1.0", override: true},
-      {:xturn_sockets,
-       git: "https://github.com/xirsys/xturn-sockets", tag: "v0.1.0", override: true},
-      {:xturn_cache, git: "https://github.com/xirsys/xturn-cache", tag: "v0.1.0"},
+      {:xmedialib, "~> 0.1.0"},
+      {:xturn_sockets, "~> 0.1.0"},
+      {:xturn_cache, "~> 0.1.0"},
       {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.18.3"},
       {:exts, "~> 0.3.4"}
