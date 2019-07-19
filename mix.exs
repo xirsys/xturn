8f252d0efb05481084102bc4bee3fb919b6162be
Lee Sylvester <lee.sylvester@gmail.com>
2019-07-19 16:42:09 +0100

    Package for hex.pm deploy

--- mix.exs
+++ mix.exs
@@ -4,12 +4,21 @@ defmodule Xirsys.XTurn.Mixfile do
   def project() do
     [
       app: :xturn,
-      version: "0.1.0",
-      elixir: ">= 1.6.6",
       name: "xturn",
+      version: "0.1.0",
+      elixir: "~> 1.7",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      description: "Xirsys XTurn TURN Server.",
       source_url: "https://github.com/xirsys/xturn",
+      homepage_url: "https://xturn.me",
+      package: package(),
+      docs: [
+        extras: ["README.md", "LICENSE.md"],
+        main: "readme"
+      ],
       escript: [main_module: Xirsys.XTurn],
-      deps: deps()
     ]
   end
 
@@ -33,8 +42,17 @@ defmodule Xirsys.XTurn.Mixfile do
       {:xturn_sockets, "~> 0.1.0"},
       {:xturn_cache, "~> 0.1.0"},
       {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
-      {:ex_doc, "~> 0.18.3"},
+      {:ex_doc, ">= 0.0.0", only: :dev},
       {:exts, "~> 0.3.4"}
     ]
+  end
+
+  defp package do
+    %{
+      files: ["lib", "mix.exs", "README.md", "LICENSE.md", "CHANGELOG.md"],
+      maintainers: ["Jahred Love"],
+      licenses: ["Apache 2.0"],
+      links: %{"Github" => "https://github.com/xirsys/xturn"}
+    }
   end
 end
